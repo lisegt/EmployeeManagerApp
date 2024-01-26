b399c209e4d8d9e088333480b30f249f5f5af371
Lise Gauthier <lise-gauthier@live.fr>
2024-01-26 19:06:56 +0100

    employee repo + service + addEmployee function

---
+++ backend/src/main/java/com/example/employeemanager/service/EmployeeService.java
@@ -0,0 +1,24 @@
+package com.example.employeemanager.service;
+
+import com.example.employeemanager.model.Employee;
+import com.example.employeemanager.repo.EmployeeRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.UUID;
+
+@Service
+public class EmployeeService {
+    private final EmployeeRepo employeeRepo;
+
+    @Autowired
+    public EmployeeService(EmployeeRepo employeeRepo) {
+        this.employeeRepo = employeeRepo;
+    }
+
+    // add en employee in the database
+    public Employee addEmployee(Employee employee) {
+        employee.setEmployeeCode(UUID.randomUUID().toString());
+        return employeeRepo.save(employee);
+    }
+}
