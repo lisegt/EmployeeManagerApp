3499a1650c1478433a9194f97dc0413cb2a5ef4a
Lise Gauthier <lise-gauthier@live.fr>
2024-01-27 12:11:49 +0100

    HttpClientModule import

--- frontend/employeemanagerapp/src/app/app.module.ts
+++ frontend/employeemanagerapp/src/app/app.module.ts
@@ -3,10 +3,11 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { EmployeeService } from './employee.service';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule],
+  imports: [BrowserModule, HttpClientModule],
   providers: [EmployeeService],
   bootstrap: [AppComponent],
 })
